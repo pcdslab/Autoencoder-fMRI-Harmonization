f64d891bcfa8af1ea9ef4b84362e54daeb8437ef
Fahad Almuqhim <f.almuqhim@gmail.com>
2025-01-24 12:39:56 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import torch
+import random
+import numpy as np
+
+SEED = 42
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+random.seed(SEED)
+np.random.seed(SEED)
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Default paths
+DATA_PATH = "/disk/raptor-array/SaeedLab-Data/Neuroimaging/ASD/fmri_rois_200"
+PHENO_PATH = "disk/raptor-array/SaeedLab-Data/Neuroimaging/ASD/Phenotypic_V1_0b_preprocessed1.csv"
+SAMPLE_PATH = "disk/raptor-array/SaeedLab-Data/Neuroimaging/ASD/10_CV_1035.csv"
